295094ccbf7ce63cbf9dcd12e0627bbb3d0e181e
Jose <joseoctavioconti@hotmail.com>
2020-06-09 02:06:47 -0300

    Initializing SubareaCard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,27 +1,48 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 
-import { Container, Machine } from './styles';
+import { Container, SubareaCard } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <>
-      <Header title="Administração - Máquinas" />
+      <Header title="Subáreas" />
 
       <Container>
-        <Machine>
-          <strong>nome da maquina</strong>
-          <p>descricao</p>
-        </Machine>
-        <Machine>
-          <strong>nome da maquina</strong>
-          <p>descricao</p>
-        </Machine>
-        <Machine>
-          <strong>nome da maquina</strong>
-          <p>descricao</p>
-        </Machine>
+        <Link to="/">
+          <img src="https://s.dicio.com.br/subarea.jpg" alt="" />
+          <SubareaCard>
+            <p>Tag: 123819</p>
+            <p>Local:</p>
+            <p>Observações:</p>
+            <p>Setor:</p>
+            <p>Cadastrado por:</p>
+          </SubareaCard>
+        </Link>
+
+        <Link to="/">
+          <img src="https://s.dicio.com.br/subarea.jpg" alt="" />
+          <SubareaCard>
+            <p>Tag: 123819</p>
+            <p>Local:</p>
+            <p>Observações:</p>
+            <p>Setor:</p>
+            <p>Cadastrado por:</p>
+          </SubareaCard>
+        </Link>
+
+        <Link to="/">
+          <img src="https://s.dicio.com.br/subarea.jpg" alt="" />
+          <SubareaCard>
+            <p>Tag: 123819</p>
+            <p>Local:</p>
+            <p>Observações:</p>
+            <p>Setor:</p>
+            <p>Cadastrado por:</p>
+          </SubareaCard>
+        </Link>
       </Container>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,14 +3,36 @@ import styled from 'styled-components';
 export const Container = styled.div`
   max-width: 1120px;
   margin: 64px auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 
-  background-color: #fff;
+  a {
+    & + a {
+      margin-top: 16px;
+    }
+
+    background-color: #3e3b47;
+    display: flex;
+    align-items: center;
+    border-radius: 8px;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      margin: 16px;
+      width: 200px;
+      height: 200px;
+      border-radius: 50%;
+    }
+  }
 `;
 
-export const Machine = styled.div`
-  flex: 1;
-  background-color: #3e3b47;
+export const SubareaCard = styled.div`
+  color: #fff;
+
+  p + p {
+    margin-top: 16px;
+  }
 `;
